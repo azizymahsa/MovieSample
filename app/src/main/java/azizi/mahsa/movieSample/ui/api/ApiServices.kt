a6f69bb82cf1e51f293119509fa5d4768935a0dd
Mahsa Azizy <37101768+azizymahsa@users.noreply.github.com>
2024-04-23 21:31:19 -0700

    Create api service for top list of Home fragment

--- app/src/main/java/azizi/mahsa/movieSample/ui/api/ApiServices.kt
+++ app/src/main/java/azizi/mahsa/movieSample/ui/api/ApiServices.kt
@@ -1,12 +1,18 @@
 package azizi.mahsa.movieSample.ui.api
 
+import azizi.mahsa.movieSample.ui.models.home.ResponseMoviesList
 import azizi.mahsa.movieSample.ui.models.register.BodyRegister
 import azizi.mahsa.movieSample.ui.models.register.ResponseRegister
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiServices {
     @POST("register")
     suspend fun registerUser(@Body body: BodyRegister): Response<ResponseRegister>
+
+    @GET("genres/{genre_id}/movies")
+    suspend fun moviesTopList(@Path("genre_id") id: Int): Response<ResponseMoviesList>
 }
